a2fce5dd2ac0d326bd00d08ae13403e3b173d253
owendev1099 <owendev1099@gmail.com>
2026-03-24 19:25:48 -0600

    Configurando CI/CD completo4

--- index.js
+++ index.js
@@ -1,11 +1,47 @@
-const express = require('express');
+const express = require("express");
+const client = require("prom-client"); // Cliente de Prometheus
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>¡Servidor de Producción Funcionando!</h1><p>Login básico activo.</p>');
+// 1. Habilitar métricas por defecto de Node.js (Memoria RAM del contenedor, CPU del proceso)
+client.collectDefaultMetrics();
+
+// 2. Crear métricas personalizadas de la aplicación
+const httpRequestCounter = new client.Counter({
+  name: "http_requests_total",
+  help: "Total de peticiones HTTP procesadas",
+  labelNames: ["metodo", "ruta", "estado_http"],
 });
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Servidor escuchando en el puerto ${port}`);
+const activeUsersGauge = new client.Gauge({
+  name: "active_users_current",
+  help: "Número actual de usuarios activos simulados",
+});
+
+// Middleware para contar peticiones
+app.use((req, res, next) => {
+  res.on("finish", () => {
+    httpRequestCounter.inc({
+      metodo: req.method,
+      ruta: req.path,
+      estado_http: res.statusCode,
+    });
+  });
+  next();
+});
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>¡Servidor de Producción Funcionando!</h1><p>Login básico activo.</p>",
+  );
+});
+
+// 3. RUTA VITAL: Endpoint /metrics donde Prometheus leerá los datos
+app.get("/metrics", async (req, res) => {
+  res.set("Content-Type", client.register.contentType);
+  res.send(await client.register.metrics());
+});
+
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
